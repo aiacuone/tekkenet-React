d9cc405c42d6863377c246ac5f16ed9a1ccea597
aiacuone <aiacuone@users.noreply.github.com>
2020-10-12 21:16:07 +0100

    startNfinish height function is done, working on specific frame, trying to merge handle changes

--- src/SpecificInputs.js
+++ src/SpecificInputs.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function SpecificInputs(props) {
-// console.log(props)
+
+let [input,setInput]= useState('')
+
+
+function handleChange(e){
+ setInput(e.target.value)
+
+}
+/** startNFinish **/
+let [startNFinish1, setStartNFinish1] =useState('')
+let [startNFinish2, setStartNFinish2] =useState('')
+
+function startNFinish(e){
+    e.target.name==='StartNFinish1'? setStartNFinish1(e.target.value):setStartNFinish2(e.target.value)
+ }
     return (
         <div>
-            {props.specificInput===''?null:console.log(props.specificInput)}
+            
+
+            {props.specificInput==='Specific Height'&&<input onChange={handleChange} type='number'></input>}
+            {props.specificInput==='Start & Finish Height'&&
+                <form>
+                    <input 
+                        onChange={startNFinish} 
+                        name='StartNFinish1'
+                        type='number'>
+                    </input>
+                    <input 
+                        onChange={startNFinish} 
+                        name='StartNFinish2'
+                        type='number'>
+                    </input>
+                </form>
+            }
+            {startNFinish1 !=='' && startNFinish2!==''? <h1>startNFinish works!</h1>:null}
         </div>
     )
 }
