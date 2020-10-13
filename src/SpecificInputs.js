a44d8ceab3dd3c7b2579af2444ba104b92e06e4e
aiacuone <aiacuone@users.noreply.github.com>
2020-10-13 23:05:50 +0100

    hopefully this works

--- src/SpecificInputs.js
+++ src/SpecificInputs.js
@@ -1,8 +1,11 @@
+
 import React from 'react'
 import SpecificHeight from './specificInputs/SpecificHeight'
 import StartNFinish from './specificInputs/StartNFinish'
 import SpecificFrame from './specificInputs/SpecificFrame'
 import FrameRange from './specificInputs/FrameRange'
+
+
 
 
 export default function SpecificInput({value}) {
@@ -13,6 +16,8 @@ export default function SpecificInput({value}) {
             {value==='Start & Finish Height'&&<StartNFinish />}
             {value==='Specific Frame'&&<SpecificFrame />}
             {value==='Frame Range'&&<FrameRange />}
+
+
         </div>
     )
 }
