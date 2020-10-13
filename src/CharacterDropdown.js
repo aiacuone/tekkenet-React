51ae68385478bcf26ddc7496651eb8a13545eeef
aiacuone <aiacuone@users.noreply.github.com>
2020-10-13 17:24:51 +0100

    restart

--- src/CharacterDropdown.js
+++ src/CharacterDropdown.js
@@ -1,36 +1,9 @@
-import React, {useEffect,useState} from 'react'
-import FirstButtonLayer from './FirstButtonLayer'
+import React from 'react'
 
-function CharacterDropdown(){
-    let characterArr= 
-    [
-        '','Akuma','Alisa','Anna','Armor King','Asuka','Bob','Bryan','Claudio','Devil Jin','Dragunov','Eddy','Eliza','Fahkumram','Feng','Ganryu','Geese','Gigas','Heihachi','Hwoarang','Jack','Jin','Josie','Julia','Katarina','Kazumi','Kazuya','King','Kuma','Lars','Law','Lee','Lei','Leo','Leroy','Lili','LuckyChoe','Marduk','MasterRaven','Miguel','Negan','Nina','Noctis','Paul','Shaheen','Steve','Xiaoyu','Yoshimitsu','Zafina'
-    ]
-
-    let charOptions= characterArr.map((char)=>{
-        return <option key = {char} value={char}>{char.toUpperCase()}</option>
-    })
- 
-    /** USE STATE HOOKS**/
-    let [character,setCharacter]=useState('')
-
-    function characterChange(e){
-        setCharacter(character=e.target.value)   
-    }
-
-    useEffect(()=>{
-        
-    },[character])
-    
-    return(
+export default function CharacterDropdown() {
+    return (
         <div>
-            <label htmlFor='characterDropdown'>CHOOSE YOUR CHARACTER</label>
-                <select name='characterDropdown' onChange={characterChange} value={character}>
-                  {charOptions}
-                </select>
-                {character===''? null :<FirstButtonLayer character={character} />}
-  
+            
         </div>
     )
 }
-export default CharacterDropdown;
